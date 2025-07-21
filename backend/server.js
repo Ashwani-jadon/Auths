8ab53ffff14772d11c1d6723e517e80372b56f4e
Ashwani-jadon <jadonashwani04@gmail.com>
2025-07-21 14:55:57 +0530

    enable cors for frontend

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,19 @@ import dotenv from "dotenv"
 import connectDb from "./config/db.js";
 import authRouter from "./Routes/user.route.js"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 const server=express()
 
 // middlewares
 server.use(express.json());
 server.use(cookieParser());  //alow to parse cookie and chek auth 
-const port=process.env.PORT;
+server.use(cors({
+    origin: "http://localhost:5173", // React app origin
+    credentials: true,               // allows cookies to be sent
+}));
 
+const port=process.env.PORT;
 server.use("/api/v1/user",authRouter);
 server.listen(port,()=>{
     connectDb();
